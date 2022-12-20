5abfaf0eab7d4261cc53b32023bfba2386b317ed
Goodl3oyZ <panudetzx2@gmail.com>
2022-12-20 16:09:00 +0700

    sedla8.3

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -2,9 +2,17 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+	if(x=='A')
+	{
+	    return 'Z';
+	}
+	if(x>='B' && x<='Z'){
+	    return x-1;
+    }else
+    {
+        return '0';
+    }
 }
-
 int main(){
 	//Test Case
 	cout << before('A') << "\n";
