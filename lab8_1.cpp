e7c1bf11144d00574be383bf518b27f83e051d1d
Goodl3oyZ <panudetzx2@gmail.com>
2022-12-20 14:38:38 +0700

    completed8.1

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
-
+#include <cmath>
+using namespace std;
 int main() {
-    int a  =  20; b = 30;
-    int c = 4, output;
+    double a  =  20, b = 30;
+    double c = 4, output;
 
-    outpot = a - b/c;
-    count << output: << output << \n;
+    output = a - b/c;
+    cout << "output: "<< output << "\n";
     return 0;
 }
